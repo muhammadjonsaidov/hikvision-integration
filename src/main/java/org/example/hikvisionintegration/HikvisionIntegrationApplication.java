727d218c47f078f2151f3388402e261a715e91b5
muhammadjonsaidov <javascript0222@gmail.com>
2025-10-14 16:52:14 +0500

    🎉 init(project): Bootstrap Spring Boot application for Hikvision integration

---
+++ src/main/java/org/example/hikvisionintegration/HikvisionIntegrationApplication.java
@@ -0,0 +1,13 @@
+package org.example.hikvisionintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikvisionIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikvisionIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hikvisionintegration/HikvisionIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hikvisionintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikvisionIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
